fdcecaa416036685fc9874f96b4e2e70f9467de8
arturvelk <64686836+arturvelk@users.noreply.github.com>
2022-04-20 14:17:17 +0200

    asd

--- app.py
+++ app.py
@@ -7,6 +7,5 @@ df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 
 df2 = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": ["asd", "sad", "das"]})
 
-st.write(df2)
 
-st.write(df)
+st.write(df2)
