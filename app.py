91126bbf92295e7cc7a7516e9330367b90c64cea
arturvelk <64686836+arturvelk@users.noreply.github.com>
2022-04-20 14:08:43 +0200

    asdasd

--- app.py
+++ app.py
@@ -4,4 +4,9 @@ import pandas as pd
 st.write("My First Streamlit Web App")
 
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
+
+df2 = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": ["asd", "sad", "das"]})
+
+st.write(df2)
+
 st.write(df)
